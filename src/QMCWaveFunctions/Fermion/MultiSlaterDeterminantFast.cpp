6d8e89be2dad56ca8133e42982a173f51fa154d3
Shiv Upadhyay <shivnupadhyay@gmail.com>
2021-02-18 04:14:41 -0500

    Use C_otherDs

--- src/QMCWaveFunctions/Fermion/MultiSlaterDeterminantFast.cpp
+++ src/QMCWaveFunctions/Fermion/MultiSlaterDeterminantFast.cpp
@@ -187,20 +187,12 @@ WaveFunctionComponent::PsiValueType MultiSlaterDeterminantFast::evaluate_vgl_imp
         const size_t spin = spinC[i];
         const size_t N                   = Dets[j]->FirstIndex;
         const size_t NP                  = Dets[j]->NumPtcls;
-        ValueType c_spin = c;
-        for (size_t k = 0; k < nd; ++k){
-            if (j != k ){
-              const auto& otherspinC  = (*C2node)[k];
-              const size_t otherspin = otherspinC[i];
-              const ValueVector_t& detValues_otherspin = Dets[k]->detValues;
-              c_spin *= detValues_otherspin[otherspin];
-            }
-        }
+        const int det_id = getDetID(iat);
         spin_psi *= detValues_spin[spin];
         for (int k = 0, n = N; k < NP; k++, n++)
         {
-          g_tmp[n] += c_spin * grads_spin(spin, k); //myG[n] += c*grads_spin(spin,k)*(detValues_otherspin[otherspin]...);
-          l_tmp[n] += c_spin * lapls_spin(spin, k); //myL[n] += c*lapls_spin(spin,k)*(detValues_otherspin[otherspin]...);
+          g_tmp[n] += C_otherDs[det_idx][i] * grads_spin(spin, k); //myG[n] += c*grads_spin(spin,k)*(detValues_otherspin[otherspin]...);
+          l_tmp[n] += C_otherDs[det_idx][i] * lapls_spin(spin, k); //myL[n] += c*lapls_spin(spin,k)*(detValues_otherspin[otherspin]...);
         }
     }
     psi += spin_psi;
